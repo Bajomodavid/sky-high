9d3af71331261951831cb72fa1b7a2cc34943409
Bajomo David <bajomomayowa@gmail.com>
2021-03-08 17:59:41 +0100

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  application_name: 'SkyHigh Limited',
+  api: {
+    version: '1.0',
+    url: 'https://g54qw205uk.execute-api.eu-west-1.amazonaws.com/DEV/stub',
+  }
 };
